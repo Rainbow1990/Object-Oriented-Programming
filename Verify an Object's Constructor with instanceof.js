7839eadacbb896270cad7c8cdc28cdd4829d54f3
Rainbow1990 <Rainbow10222@gmail.com>
2022-12-21 11:18:21 +0300

    Object Oriented Programming

---
+++ Verify an Object's Constructor with instanceof.js
@@ -0,0 +1,11 @@
+function House(numBedrooms) {
+    this.numBedrooms = numBedrooms;
+  }
+  
+  // Only change code below this line
+  
+    let myHouse = new House(4);
+    let secondHouse = { numRooms: 5 };
+  
+    console.log(myHouse instanceof House);
+    console.log(secondHouse instanceof House);
